dea8102e48c813e1463ba594bdce5fdb8e74192d
Md Ishmam Iqbal <mdishmamiqbal@Mds-MacBook-Pro.local>
2023-03-04 12:09:58 +0600

    fixed routing error

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+
 import Home from "./pages/Home";
-import Helmet from "./components/Helmet";
+import Products from "./pages/Products";
 
 const App = () => {
   return (
-    <div>
-      <Helmet />
-      <Home />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/:category" element={<Products />} />
+    </Routes>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,25 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./App.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  BrowserRouter,
+} from "react-router-dom";
+import App from "./App";
 
-import Home from "./pages/Home";
-import Products from "./pages/Products";
+// import Home from "./pages/Home";
+// import Products from "./pages/Products";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/:category",
-    element: <Products />,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Home />,
+//   },
+//   {
+//     path: "/:category",
+//     element: <Products />,
+//   },
+// ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
